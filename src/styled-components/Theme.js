4e8fa44e89fb4569ef3c7329f149ff7d352361a0
David Sotto Mayor <ddsmmf@gmail.com>
2021-08-16 15:01:13 -0400

    Setting up the project

---
+++ src/styled-components/Theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    name: 'The standart theme!',
+    primary: '#284b63',
+    secondary: '#3c6e71',
+    tertiary: '#353535',
+    background: '#d9d9d9',
+    text: '#ffffff',
+
+}
