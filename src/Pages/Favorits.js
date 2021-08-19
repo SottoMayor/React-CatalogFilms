4b9dfa59d3d7e6d62316e001b3fc81e0d465c6e0
David Sotto Mayor <ddsmmf@gmail.com>
2021-08-19 16:00:43 -0400

    Deleting (definitely) films in localStorage

--- src/Pages/Favorits.js
+++ src/Pages/Favorits.js
@@ -1,15 +1,22 @@
 import React, {useState, useEffect } from 'react';
 import Film from '../Components/Film';
+import AlertStatus from '../Components/UI/AlertStatus';
 
 const Favorits = () => {
 
-    const [favorits, setFavorits] = useState([])
+    const [favorits, setFavorits] = useState([]);
+
     
     // Getting films of the localStorage
     useEffect(() => {
         const favoritFilms = localStorage.getItem('filmes');
         setFavorits(JSON.parse(favoritFilms));
     }, []);
+
+    // Updating films of the localStorage
+    useEffect(() => {
+        localStorage.setItem('filmes', JSON.stringify(favorits));
+    }, [favorits]);
 
     
     const deleteFilmHandler = (filmId) => {
@@ -19,7 +26,9 @@ const Favorits = () => {
 
     return (
         <div>
-            {favorits.map( film => <Film {...film} key={film.id} onDeleteFilm={deleteFilmHandler} favorits/>)}
+            <h2 style={{'textAlign': 'center', 'margin-bottom': '15px'}}>Meus Filmes Favoritos!</h2>
+            {favorits.length > 0 && favorits.map( film => <Film {...film} key={film.id} onDeleteFilm={deleteFilmHandler} favorits/>)}
+            {favorits.length === 0 && <AlertStatus status='warning' hideButton> Você não possui filmes favoritos! </AlertStatus>}
         </div>
     )
 }
