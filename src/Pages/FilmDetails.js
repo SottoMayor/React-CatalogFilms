44bd131870079c138c08e0dd127ee41e8f6a7b0d
David Sotto Mayor <ddsmmf@gmail.com>
2021-08-19 13:06:58 -0400

    Finishing the FilmDetails and storing favorite films in localStorage

--- src/Pages/FilmDetails.js
+++ src/Pages/FilmDetails.js
@@ -1,16 +1,24 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { alertAction } from '../store';
 import axios from '../axios';
 
 import FilmCard from '../Components/FilmCard'
+import Alert from '../Layout/Alert';
+import AlertStatus from '../Components/UI/AlertStatus';
 
 const FilmDetails = () => {
+
+    const dispatch = useDispatch();
+    const showAlert = useSelector(state => state.alert.show)
 
     const { filmId } = useParams();
 
     const [film, setFilm] = useState({});
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [alertError, setAlertError] = useState(null);
     const [message, setMessage] = useState('');
     
     const loadingFilmsHandler = useCallback( async () => {
@@ -41,12 +49,35 @@ const FilmDetails = () => {
     
 
     const favoriteHandler = () => {
+        setAlertError(null);
+
+        const list = localStorage.getItem("filmes");
+
+        let salvedList = JSON.parse(list) || [];
+
         
-        localStorage.setItem('filmes', JSON.stringify(film))
-    }
+        const foundFilm = salvedList.findIndex(salvedFilm => salvedFilm.id.toString() === filmId.toString());
+        if(foundFilm !== -1){
+            setAlertError(true);
+            setMessage(`${film.nome} já está nos seus favoritos!`);
+            dispatch(alertAction.showAlert());
+            return;
+        }
+        
+
+        salvedList.push(film);
+        localStorage.setItem('filmes', JSON.stringify(salvedList));
+        setMessage(`Você favoritou ${film.nome}`);
+        dispatch(alertAction.showAlert());
+
+        }
+        
     
     return (
         <div>
+            <Alert show={showAlert} clicked={() => { dispatch(alertAction.closeAlert()); }}>
+                <AlertStatus status={alertError ? 'warning' : 'success'}>{message}</AlertStatus>
+            </Alert>
             <FilmCard onFavorite={favoriteHandler} film={film} loading={loading} error={error} message={message}/>
         </div>
     )
